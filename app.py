3211a96865a3971f233a4c2b13f56045fb1f65b4
turtlecoco <turtlecoco88@gmail.com>
2025-02-12 08:37:04 +0000

    1

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ from flask import render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
         return(render_template("index.html"))
+
+@app.route("/Prediction", methods=["GET","POST"])
+def prediction():
+        return(render_template("Prediction.html"))
+
+@app.route("/Predicted_DBS", methods=["GET","POST"])
+def prediction_DBS():
+        q = float(request.form.get("q"))
+        return(render_template("Predicted_DBS.html",r=(-50.6*q)+90.2))
 
 if __name__ == "__main__":
         app.run()
